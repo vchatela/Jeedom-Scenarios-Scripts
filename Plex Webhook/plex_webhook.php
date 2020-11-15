4a3957efa1817ba62fac2240994390b1253c292f
Valentin C <chatelard.valentin@gmail.com>
2020-11-15 14:34:31 +0100

    Multi users

--- Plex Webhook/plex_webhook.php
+++ Plex Webhook/plex_webhook.php
@@ -25,6 +25,57 @@ function send_notification($message){
     $notif_scenario->launch(); 
 }
 
+function notif_event($account_id, $username){
+	$message_stop="[OK] Plex disponible.";
+	$message_start="[Occupé] $username regarde : ".$payload["Metadata.title"];
+	if(startsWith($payload["Metadata.guid"],"tv.plex.xmltv") || startsWith($payload["Metadata.key"],"/livetv/")){
+		switch ($payload["event"]){
+			case "media.stop":
+				#send_notification($message_stop);
+				send_to_queue($message_stop);
+				$scenario->setData("plex_running_$account_id",0);
+				#$scenario->setLog('event : media.stop raised');
+				break;
+			case "media.play":
+				#send_notification($message_start);
+				send_to_queue($message_start);
+				$scenario->setData("plex_running_$account_id",1);
+				#$scenario->setLog('event : media.play raised');
+				$scenario->setLog($payload["Metadata.title"] . "is being played");
+				break;
+			case "media.pause":
+				# que faire de pause ? rien ?
+				if($scenario->getData("plex_running_$account_id") == 1){
+					$scenario->setData("plex_running_$account_id",0);
+					#send_notification($message_stop);
+					send_to_queue($message_stop);
+				}
+				#$scenario->setLog('event : media.pause raised');
+				break;
+			case "media.resume":
+				# que faire de resume ? rien ?
+				if($scenario->getData("plex_running_$account_id") == 0){
+					$scenario->setData("plex_running_$account_id",1);
+					#send_notification($message_start);
+					send_to_queue($message_start);
+				}
+				#$scenario->setLog('event : media.resume raised');
+				break;
+			case "media.scrobble":
+				# que faire de resume ? rien ?
+				if($scenario->getData("plex_running_$account_id") == 0){
+					$scenario->setData("plex_running_$account_id",1);
+					#send_notification($message_start);
+					send_to_queue($message_start);
+				}
+				#$scenario->setLog('event : media.scrobble raised');
+				break;
+		  default:
+				$scenario->setLog('event not known... : event='. $payload["event"]);
+				break;
+		}
+	}
+}
 
 function send_to_queue($message){
   global $scenario;
@@ -39,7 +90,8 @@ function send_to_queue($message){
 
 
 $known_values = array();
-$known_values["mibox_toulouse.uuid"] = "106e1da57d2ea0ff-com-plexapp-android";
+$known_values["mibox_toulouse.uuid"] = "XXXXXXXXX-com-plexapp-android";
+$known_values["mibox_s_lescar.uuid"] = "YYYYYYYYY-com-plexapp-android";
 
 // On récupère le payload
 $cmd = cmd::byString("#[Equipements][Plex Webhook][payload]#");
@@ -63,59 +115,14 @@ $payload["Metadata.type"] = $results->Metadata->type;
 $payload["Metadata.key"] = $results->Metadata->key;
 $payload["Metadata.guid"] = $results->Metadata->guid;
 
-$message_stop="[OK] Plex disponible.";
-$message_start="[Occupé] Valentin regarde : ".$payload["Metadata.title"];
-
-if(strcmp($payload["Server.title"],"synology_vc") == 0 || strcmp($payload["Server.uuid"],"a626911308c33cf28a2bc4f7465d41360e105b83") == 0){
+if(strcmp($payload["Server.title"],"synology_vc") == 0 || strcmp($payload["Server.uuid"],"XX_IDSERVER") == 0){
   #$scenario->setLog('Running on Plex NAS..');
-  if($payload["Account.id"] == 1 || strcmp($payload["Account.title"],"chatelard.valentin")==0){
-    #$scenario->setLog('Valentin is playing ..');
-  	if(startsWith($payload["Metadata.guid"],"tv.plex.xmltv") || startsWith($payload["Metadata.key"],"/livetv/")){
-   		switch ($payload["event"]){
-         	case "media.stop":
-        		send_notification($message_stop);
-				send_to_queue($message_stop);
-            	$scenario->setData('plex_running',0);
-            	$scenario->setLog('event : media.stop raised');
-        		break;
-           	case "media.play":
-        		send_notification($message_start);
-				send_to_queue($message_start);
-            	$scenario->setData('plex_running',1);
-            	$scenario->setLog('event : media.play raised');
-            	$scenario->setLog($payload["Metadata.title"] . "is being played");
-        		break;
-            case "media.pause":
-        		# que faire de pause ? rien ?
-				if($scenario->getData('plex_running') == 1){
-            		$scenario->setData('plex_running',0);
-                  	send_notification($message_stop);
-					send_to_queue($message_stop);
-                }
-            	$scenario->setLog('event : media.pause raised');
-        		break;
-            case "media.resume":
-        		# que faire de resume ? rien ?
-            	if($scenario->getData('plex_running') == 0){
-            		$scenario->setData('plex_running',1);
-                  	send_notification($message_start);
-					send_to_queue($message_start);
-                }
-            	$scenario->setLog('event : media.resume raised');
-        		break;
-            case "media.scrobble":
-        		# que faire de resume ? rien ?
-            	if($scenario->getData('plex_running') == 0){
-            		$scenario->setData('plex_running',1);
-                  	send_notification($message_start);
-					send_to_queue($message_start);
-                }
-            	$scenario->setLog('event : media.scrobble raised');
-        		break;
-          default:
-            	$scenario->setLog('event not known... : event='. $payload["event"]);
-            	break;
-        }
-  	}
-  }
+	if($payload["Account.id"] == XX_ACCOUNT_ID_1 || strcmp($payload["Account.title"],"XX_ACCOUNT_1")==0){
+		#$scenario->setLog('$account_title is playing ..');
+		notif_event($payload["Account.id"],"Valentin");
+	} else {
+		if($payload["Account.id"] == XX_ACCOUNT_ID_2 || strcmp($payload["Account.title"],"XX_ACCOUNT_2")==0){
+			#notif_event($payload["Account.id"],"Fifi");
+		}
+	}
 }
